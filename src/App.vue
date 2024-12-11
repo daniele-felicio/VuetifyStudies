92027d09c6be169d1ded019578301fc854f5c87a
Daniele Felicio <daniele.felicior@gmail.com>
2024-12-11 17:56:17 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,14 @@
     <v-app-bar flat class="border-b">
       <v-app-bar-title>Meu App</v-app-bar-title>
     </v-app-bar>
-
     <v-main>
       <v-container>
-<h1 class="hidden-md text-h5 text-md-h1">DashBoard</h1>
+        <h1 class="hidden-md text-h5 text-md-h1">DashBoard</h1>
 
         <div class="d-flex flex-column flex-md-row justify-space-between">
-          <div
-            class="w-100 pa-4 border rounded-lg elevation-3">
-            teste
-          </div>
-          <div
-            class="w-100 pa-4 border rounded-lg elevation-3">
-            teste
-          </div>
-          <div
-            class="w-100 pa-4 border rounded-lg elevation-3">
-            teste
-          </div>
+          <div class="w-100 pa-4 border rounded-lg elevation-3">teste</div>
+          <div class="w-100 pa-4 border rounded-lg elevation-3">teste</div>
+          <div class="w-100 pa-4 border rounded-lg elevation-3">teste</div>
         </div>
       </v-container>
 
@@ -30,5 +20,5 @@
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
