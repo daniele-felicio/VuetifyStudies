472fe68955cd5110a94fb192f57d7636145f07e5
Daniele Felicio <daniele.felicior@gmail.com>
2024-12-11 22:25:52 -0300

    Aula de quarta

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,93 @@
     </v-app-bar>
     <v-main>
       <v-container>
-        <h1 class="hidden-md text-h5 text-md-h1">DashBoard</h1>
+        <h1>Dashboard</h1>
+        <v-row>
+          <v-col cols="12" md="4" lg="3">
+            <v-card flat class="border">
+              <v-img
+                class="align-end text-white"
+                src="https://media.istockphoto.com/id/1294939081/pt/foto/reforestation-eucalyptus-in-the-greenhouse-growing.jpg?s=2048x2048&w=is&k=20&c=c7Y8pZEgIz4zivChjbsrwrkA-PYya6r5b2gBB5mpfkI="
+              >
+                <v-card-title class="text-sm-subtitle-1"
+                  >Materiais Genéticos Clonais</v-card-title
+                >
+              </v-img>
 
-        <div class="d-flex flex-column flex-md-row justify-space-between">
-          <div class="w-100 pa-4 border rounded-lg elevation-3">teste</div>
-          <div class="w-100 pa-4 border rounded-lg elevation-3">teste</div>
-          <div class="w-100 pa-4 border rounded-lg elevation-3">teste</div>
-        </div>
+              <v-card-subtitle class="pt-4">Eucalipto</v-card-subtitle>
+
+              <v-card-text>
+                <div>AEC0144</div>
+                <div>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer nec odio. Praesent libero. Sed cursus ante dapibus
+                  diam.
+                </div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn>Ver mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" md="4" lg="3">
+            <v-card flat class="border">
+              <v-img
+                class="align-end text-white"
+                src="https://media.istockphoto.com/id/1294939081/pt/foto/reforestation-eucalyptus-in-the-greenhouse-growing.jpg?s=2048x2048&w=is&k=20&c=c7Y8pZEgIz4zivChjbsrwrkA-PYya6r5b2gBB5mpfkI="
+              >
+                <v-card-title class="text-sm-subtitle-1"
+                  >Materiais Genéticos Clonais</v-card-title
+                >
+              </v-img>
+
+              <v-card-subtitle class="pt-4">Eucalipto</v-card-subtitle>
+
+              <v-card-text>
+                <div>AEC0144</div>
+                <div>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer nec odio. Praesent libero. Sed cursus ante dapibus
+                  diam.
+                </div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn>Ver mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" md="4" lg="3">
+            <v-card flat class="border">
+              <v-img
+                class="align-end text-white"
+                src="https://media.istockphoto.com/id/1294939081/pt/foto/reforestation-eucalyptus-in-the-greenhouse-growing.jpg?s=2048x2048&w=is&k=20&c=c7Y8pZEgIz4zivChjbsrwrkA-PYya6r5b2gBB5mpfkI="
+              >
+                <v-card-title class="text-sm-subtitle-1"
+                  >Materiais Genéticos Clonais</v-card-title
+                >
+              </v-img>
+
+              <v-card-subtitle class="pt-4">Eucalipto</v-card-subtitle>
+
+              <v-card-text>
+                <div>AEC0144</div>
+                <div>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer nec odio. Praesent libero. Sed cursus ante dapibus
+                  diam.
+                </div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn>Ver mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
-
-      <HelloWorld />
     </v-main>
   </v-app>
 </template>
